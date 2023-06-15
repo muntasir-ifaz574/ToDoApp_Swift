ad0cb9e4e76a1024c978ca0612123f0cd5b9eeaa
Muntasir Ifaz <muntasir574@gmail.com>
2023-06-15 10:56:33 +0600

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Muntasir Efaz on 6/15/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
