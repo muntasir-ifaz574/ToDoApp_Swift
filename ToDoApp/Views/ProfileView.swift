1be74a51ebc016214dfae3aa521378954afbf84d
Muntasir Ifaz <muntasir574@gmail.com>
2023-06-18 15:59:37 +0600

    Show user information in profile screen

--- ToDoApp/Views/ProfileView.swift
+++ ToDoApp/Views/ProfileView.swift
@@ -13,9 +13,44 @@ struct ProfileView: View {
     var body: some View {
         NavigationView{
             VStack{
-                
+                if let user = viewModel.user{
+                    Image(systemName: "person.circle")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .foregroundColor(Color.blue)
+                        .frame(width: 125, height:  125)
+                        .padding()
+                    
+                    //Info
+                    VStack(alignment: .leading){
+                        HStack {
+                            Text("Name: ")
+                            Text(user.name)
+                        }
+                        HStack {
+                            Text("Email: ")
+                            Text(user.email)
+                        }
+                        HStack {
+                            Text("Member  Since: ")
+                            Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+                        }
+                    }
+                    .padding()
+                    //Sign out
+                    Button("Log Out") {
+                        viewModel.logout()
+                    }
+                    .tint(.red)
+                    .padding()
+                } else {
+                    Text("Loading")
+                }
             }
             .navigationTitle("Profile")
+        }
+        .onAppear{
+            viewModel.fatchUser()
         }
     }
 }
